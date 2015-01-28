5f75cb1de561c72fb66c92f348e02df7aaeb48a7
himamis <megette_a@yahoo.com>
2015-01-28 22:18:49 +0100

    first gwt files

---
+++ jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/FactoryProviderGWT.java
@@ -0,0 +1,42 @@
+package org.scilab.forge.jlatexmath;
+
+import org.scilab.forge.jlatexmath.platform.FactoryProvider;
+import org.scilab.forge.jlatexmath.platform.font.FontFactory;
+import org.scilab.forge.jlatexmath.platform.geom.GeomFactory;
+import org.scilab.forge.jlatexmath.platform.graphics.GraphicsFactory;
+import org.scilab.forge.jlatexmath.platform.parser.ParserFactory;
+import org.scilab.forge.jlatexmath.platform.resources.ResourceLoaderFactory;
+
+public class FactoryProviderGWT extends FactoryProvider {
+
+	@Override
+	protected GeomFactory createGeomFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected FontFactory createFontFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected GraphicsFactory createGraphicsFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected ParserFactory createParserFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected ResourceLoaderFactory createResourceLoaderFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
