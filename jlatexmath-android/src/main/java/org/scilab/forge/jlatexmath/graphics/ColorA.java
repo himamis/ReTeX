fd04f9e41957ea7f8194df07d58c4f785040f259
unknown <megette_a@yahoo.com>
2015-01-30 09:49:15 +0100

    Fixed android nature, jlatexmath caching

--- jlatexmath-android/src/main/java/org/scilab/forge/jlatexmath/graphics/ColorA.java
+++ jlatexmath-android/src/main/java/org/scilab/forge/jlatexmath/graphics/ColorA.java
@@ -21,4 +21,28 @@ public class ColorA implements Color {
 	public int getColor() {
 		return mColor;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + mColor;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ColorA other = (ColorA) obj;
+		if (mColor != other.mColor)
+			return false;
+		return true;
+	}
+
+	
 }
