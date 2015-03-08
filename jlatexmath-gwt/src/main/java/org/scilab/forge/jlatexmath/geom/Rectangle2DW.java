2ad33b9590922a0bb2e72a3027cee0de8b4cbf82
himamis <megette_a@yahoo.com>
2015-03-08 09:28:44 +0100

    Geom classes created

---
+++ jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/geom/Rectangle2DW.java
@@ -0,0 +1,62 @@
+package org.scilab.forge.jlatexmath.geom;
+
+import org.scilab.forge.jlatexmath.platform.geom.Rectangle2D;
+
+public class Rectangle2DW implements Rectangle2D {
+
+	private double x;
+	private double y;
+	private double width;
+	private double height;
+
+	public Rectangle2DW(double x, double y, double width, double height) {
+		setRectangle(x, y, width, height);
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public Object getNativeObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRectangle(double x, double y, double width, double height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+}
