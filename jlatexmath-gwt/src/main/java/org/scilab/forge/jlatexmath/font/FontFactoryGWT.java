f224d45f8ab566cf3bc9f5c08c56e898aec48469
himamis <megette_a@yahoo.com>
2015-03-22 13:29:34 +0100

    Created empty font factory

---
+++ jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/font/FontFactoryGWT.java
@@ -0,0 +1,36 @@
+package org.scilab.forge.jlatexmath.font;
+
+import org.scilab.forge.jlatexmath.platform.font.Font;
+import org.scilab.forge.jlatexmath.platform.font.FontFactory;
+import org.scilab.forge.jlatexmath.platform.font.FontLoader;
+import org.scilab.forge.jlatexmath.platform.font.FontRenderContext;
+import org.scilab.forge.jlatexmath.platform.font.TextAttributeProvider;
+import org.scilab.forge.jlatexmath.platform.font.TextLayout;
+
+public class FontFactoryGWT extends FontFactory {
+
+	@Override
+	public Font createFont(String name, int style, int size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TextLayout createTextLayout(String string, Font font, FontRenderContext fontRenderContext) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TextAttributeProvider createTextAttributeProvider() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FontLoader createFontLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
