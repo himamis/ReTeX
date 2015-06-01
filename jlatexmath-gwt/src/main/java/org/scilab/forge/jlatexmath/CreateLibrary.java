80082aec5cefac70db389367f5f890e9122fcac7
Bencze Balázs <megette_a@yahoo.com>
2015-06-01 10:36:57 +0200

    exporting simplified added init to predefine latex commands

--- jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/CreateLibrary.java
+++ jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/CreateLibrary.java
@@ -1,69 +1,29 @@
 package org.scilab.forge.jlatexmath;
 
-import org.scilab.forge.jlatexmath.graphics.Graphics2DW;
 import org.scilab.forge.jlatexmath.platform.FactoryProvider;
-import org.scilab.forge.jlatexmath.platform.graphics.Color;
-import org.scilab.forge.jlatexmath.platform.graphics.Graphics2DInterface;
-import org.scilab.forge.jlatexmath.platform.graphics.HasForegroundColor;
 
-import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.JavaScriptObject;
 
-/**
- * Exports the JLaTeXMath library to the Javascript world with the {@link #LIB_NAME LIB_NAME} prefix.
- */
 public class CreateLibrary implements EntryPoint {
 
-	public static final String LIB_NAME = "jlmlib";
+	private JlmLib library;
 
 	@Override
 	public void onModuleLoad() {
 		FactoryProvider.INSTANCE = new FactoryProviderGWT();
-		exportLibrary();
+		library = new JlmLib();
+		exportLibrary(library);
 	}
 
-	private native void exportLibrary() /*-{
+	private static native void exportLibrary(JlmLib library) /*-{
 		$wnd.jlmlib = {};
-		$wnd.jlmlib.drawLatex = function(ctx, latex, size, style, x, y,
-				fgColor, cb) {
-			var color = @org.scilab.forge.jlatexmath.ColorUtil::decode(Ljava/lang/String;)(fgColor);
-			@org.scilab.forge.jlatexmath.CreateLibrary::drawLatex(Lcom/google/gwt/canvas/dom/client/Context2d;Ljava/lang/String;FIIILorg/scilab/forge/jlatexmath/platform/graphics/Color;Lcom/google/gwt/core/client/JavaScriptObject;)(ctx, latex, size, style, x, y, color, cb);
-		}
-	}-*/;
-
-	public static TeXIcon createIcon(final String latex, final float size,
-			final int style) {
-		TeXFormula formula = new TeXFormula(latex);
-		TeXIcon icon = formula.new TeXIconBuilder().setStyle(style)
-				.setSize(size).build();
-
-		// icon.setInsets(new Insets(5, 5, 5, 5));
-		return icon;
-	}
-
-	public static void drawLatex(final Context2d ctx, final String latex,
-			final float size, final int style, final int x, final int y,
-			final Color fgColor, final JavaScriptObject callback) {
-		TeXIcon icon = createIcon(latex, size, style);
-		Graphics2DW g2 = new Graphics2DW(ctx);
-		g2.setDrawingFinishedCallback(new DrawingFinishedCallback() {
-			@Override
-			public void onDrawingFinished() {
-				callJavascriptCallback(callback);
-			}
+		$wnd.jlmlib.initWith = $entry(function(str) {
+			library.@org.scilab.forge.jlatexmath.JlmLib::initWith(Ljava/lang/String;)(str);
 		});
-		icon.paintIcon(new HasForegroundColor() {
-			@Override
-			public Color getForegroundColor() {
-				return fgColor;
-			}
-		}, g2, x, y);
-	}
-	
-	private static native void callJavascriptCallback(JavaScriptObject cb) /*-{
-		if (cb != null) {
-			cb();
-		}
+		$wnd.jlmlib.drawLatex = $entry(function(ctx, latex, size, style, x, y,
+				fgColor, cb) {
+			library.@org.scilab.forge.jlatexmath.JlmLib::drawLatex(Lcom/google/gwt/canvas/dom/client/Context2d;Ljava/lang/String;FIIILjava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)(ctx, latex, size, style, x, y, fgColor, cb);
+		});
 	}-*/;
+
 }
---
+++ jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/JlmLib.java
@@ -0,0 +1,71 @@
+package org.scilab.forge.jlatexmath;
+
+import org.scilab.forge.jlatexmath.graphics.Graphics2DW;
+import org.scilab.forge.jlatexmath.platform.graphics.Color;
+import org.scilab.forge.jlatexmath.platform.graphics.HasForegroundColor;
+
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.shared.GWT;
+
+public class JlmLib {
+
+	private StringBuilder initString;
+
+	public JlmLib() {
+		GWT.log("creating init string");
+		initString = new StringBuilder();
+		GWT.log("init string created");
+		if (initString == null) {
+			GWT.log("this shouldn't happen");
+		}
+	}
+
+	public void initWith(String string) {
+		initString.append(initString);
+	}
+
+	public void drawLatex(final Context2d ctx, final String latex,
+			final float size, final int style, final int x, final int y,
+			final String fgColorString, final JavaScriptObject callback) {
+		if (initString == null) {
+			GWT.log("wha?");
+		}
+		if (initString.length() > 0) {
+			new TeXFormula(initString.toString());
+			initString.setLength(0);
+		}
+		TeXIcon icon = createIcon(latex, size, style);
+		Graphics2DW g2 = new Graphics2DW(ctx);
+		g2.setDrawingFinishedCallback(new DrawingFinishedCallback() {
+			@Override
+			public void onDrawingFinished() {
+				callJavascriptCallback(callback);
+			}
+		});
+		final Color fgColor = ColorUtil.decode(fgColorString);
+		icon.paintIcon(new HasForegroundColor() {
+			@Override
+			public Color getForegroundColor() {
+				return fgColor;
+			}
+		}, g2, x, y);
+	}
+
+	private static native void callJavascriptCallback(JavaScriptObject cb) /*-{
+		if (cb != null) {
+			cb();
+		}
+	}-*/;
+
+	private static TeXIcon createIcon(final String latex, final float size,
+			final int style) {
+		TeXFormula formula = new TeXFormula(latex);
+		TeXIcon icon = formula.new TeXIconBuilder().setStyle(style)
+				.setSize(size).build();
+
+		// icon.setInsets(new Insets(5, 5, 5, 5));
+		return icon;
+	}
+
+}
