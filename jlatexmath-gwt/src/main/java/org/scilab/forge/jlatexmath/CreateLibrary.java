5c37d2debc3a45190cee62fd11d000767b9fa0f8
Bencze Balázs <megette_a@yahoo.com>
2015-06-16 08:47:39 +0200

    Added insets to library Change style to type javascript library parameters in JSON

--- jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/CreateLibrary.java
+++ jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/CreateLibrary.java
@@ -20,13 +20,50 @@ public class CreateLibrary implements EntryPoint {
 
 	private static native void exportLibrary(JlmLib library, Opentype opentype) /*-{
 		$wnd.jlmlib = {};
+
 		$wnd.jlmlib.initWith = $entry(function(str) {
 			library.@org.scilab.forge.jlatexmath.JlmLib::initWith(Ljava/lang/String;)(str);
 		});
-		$wnd.jlmlib.drawLatex = $entry(function(ctx, latex, size, style, x, y,
-				fgColor, bgColor, cb) {
-			return library.@org.scilab.forge.jlatexmath.JlmLib::drawLatex(Lcom/google/gwt/canvas/dom/client/Context2d;Ljava/lang/String;FIIILjava/lang/String;Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)(ctx, latex, size, style, x, y, fgColor, bgColor, cb);
+
+		$wnd.jlmlib.SERIF = @org.scilab.forge.jlatexmath.TeXFormula::SERIF;
+		$wnd.jlmlib.SANSSERIF = @org.scilab.forge.jlatexmath.TeXFormula::SANSSERIF;
+		$wnd.jlmlib.BOLD = @org.scilab.forge.jlatexmath.TeXFormula::BOLD;
+		$wnd.jlmlib.ITALIC = @org.scilab.forge.jlatexmath.TeXFormula::ITALIC;
+		$wnd.jlmlib.ROMAN = @org.scilab.forge.jlatexmath.TeXFormula::ROMAN;
+		$wnd.jlmlib.TYPEWRITER = @org.scilab.forge.jlatexmath.TeXFormula::TYPEWRITER;
+
+		$wnd.jlmlib.drawLatex = $entry(function(opts) {
+			//ctx, latex, size, style, x, y, fgColor, bgColor, cb
+			if (!opts.context) {
+				throw ("drawLatex(opts): opts.context must not be null");
+			}
+			if (!opts.latex) {
+				throw ("drawLatex(opts): opts.latex must not be null");
+			}
+			var ctx = opts.context, 
+			latex = opts.latex, 
+			size = opts.size || 12, 
+			type = opts.type || 0, 
+			x = opts.x || 0, 
+			y = opts.y || 0, 
+			insets = opts.insets
+					|| {
+						top : 0,
+						bottom : 0,
+						left : 0,
+						right : 0
+					}, 
+			topInset = insets.top || 0, 
+			bottomInset = insets.bottom || 0, 
+			leftInset = insets.left || 0, 
+			rightInset = insets.right || 0, 
+			fgColor = opts.foregroundColor || "#000000", 
+			bgColor = opts.backgroundColor, // undefined === invisible
+			cb = opts.callback;
+
+			return library.@org.scilab.forge.jlatexmath.JlmLib::drawLatex(Lcom/google/gwt/canvas/dom/client/Context2d;Ljava/lang/String;FIIIIIIILjava/lang/String;Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)(ctx, latex, size, type, x, y, topInset, leftInset, bottomInset, rightInset, fgColor, bgColor, cb);
 		});
+		
 		$wnd.jlmlib.setFontBaseUrl = $entry(function(url) {
 			opentype.@org.scilab.forge.jlatexmath.font.opentype.Opentype::setFontBaseUrl(Ljava/lang/String;)(url);
 		});
--- jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/JlmLib.java
+++ jlatexmath-gwt/src/main/java/org/scilab/forge/jlatexmath/JlmLib.java
@@ -3,6 +3,7 @@ package org.scilab.forge.jlatexmath;
 import org.scilab.forge.jlatexmath.graphics.Graphics2DW;
 import org.scilab.forge.jlatexmath.platform.graphics.Color;
 import org.scilab.forge.jlatexmath.platform.graphics.HasForegroundColor;
+import org.scilab.forge.jlatexmath.platform.graphics.Insets;
 
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.JavaScriptObject;
@@ -20,27 +21,27 @@ public class JlmLib {
 	}
 
 	public int[] drawLatex(final Context2d ctx, final String latex,
-			final float size, final int style, final int x, final int y,
-			final String fgColorString, final String bgColorString,
-			final JavaScriptObject callback) {
-		
+			final float size, final int type, final int x, final int y,
+			final int topInset, final int leftInset, final int bottomInset,
+			final int rightInset, final String fgColorString,
+			final String bgColorString, final JavaScriptObject callback) {
+
 		// init jlm with the given string
 		if (initString.length() > 0) {
 			new TeXFormula(initString.toString());
 			initString.setLength(0);
 		}
-		
 		// create icon and graphics objects
-		TeXIcon icon = createIcon(latex, size, style);
+		TeXIcon icon = createIcon(latex, size, type, new Insets(topInset, leftInset, bottomInset, rightInset));
 		Graphics2DW g2 = new Graphics2DW(ctx);
-		
+
 		// fill the background color
 		if (bgColorString != null && !bgColorString.equals("")) {
 			final Color bgColor = ColorUtil.decode(bgColorString);
 			g2.setColor(bgColor);
 			g2.fillRect(x, y, icon.getIconWidth(), icon.getIconHeight());
 		}
-		
+
 		// set the callback
 		g2.setDrawingFinishedCallback(new DrawingFinishedCallback() {
 			@Override
@@ -48,7 +49,7 @@ public class JlmLib {
 				callJavascriptCallback(callback);
 			}
 		});
-		
+
 		// paint the icon
 		final Color fgColor = ColorUtil.decode(fgColorString);
 		icon.paintIcon(new HasForegroundColor() {
@@ -58,7 +59,7 @@ public class JlmLib {
 			}
 		}, g2, x, y);
 		g2.maybeNotifyDrawingFinishedCallback();
-		
+
 		// return {width, height}
 		return new int[] { icon.getIconWidth(), icon.getIconHeight() };
 	}
@@ -70,10 +71,11 @@ public class JlmLib {
 	}-*/;
 
 	private static TeXIcon createIcon(final String latex, final float size,
-			final int style) {
+			final int type, Insets insets) {
 		TeXFormula formula = new TeXFormula(latex);
-		TeXIcon icon = formula.new TeXIconBuilder().setStyle(style)
+		TeXIcon icon = formula.new TeXIconBuilder().setStyle(TeXConstants.STYLE_DISPLAY).setType(type)
 				.setSize(size).build();
+		icon.setInsets(insets);
 		return icon;
 	}
 
