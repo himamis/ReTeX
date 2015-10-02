075b8dcc4f47e64cfe127aa68d2e4df494a1ed65
Bencze Balázs <megette_a@yahoo.com>
2015-10-02 08:02:17 +0200

    fixed x/5

--- editor/share/src/main/java/com/himamis/retex/editor/share/controller/InputController.java
+++ editor/share/src/main/java/com/himamis/retex/editor/share/controller/InputController.java
@@ -19,10 +19,12 @@ public class InputController {
 
     private MetaModel metaModel;
     private CursorController cursorController;
+    private ArgumentHelper argumentHelper;
 
     public InputController(MetaModel metaModel, CursorController cursorController) {
         this.metaModel = metaModel;
         this.cursorController = cursorController;
+        this.argumentHelper = new ArgumentHelper();
     }
 
     /**
@@ -148,7 +150,7 @@ public class InputController {
 
         // pass characters for fraction and factorial only
         if ("frac".equals(name) /*|| "factorial".equals(name)*/) {
-            passArgument(editorState, function);
+            argumentHelper.passArgument(editorState, function);
         }
         currentOffset = editorState.getCurrentOffset();
         currentField.addArgument(currentOffset, function);
@@ -520,71 +522,6 @@ public class InputController {
             editorState.setCurrentField(parent);
             editorState.setCurrentOffset(offset);
         }
-    }
-
-    private void passArgument(EditorState editorState, MathContainer container) {
-        MathSequence currentField = editorState.getCurrentField();
-        int currentOffset = editorState.getCurrentOffset();
-        // get pass to argument
-        MathSequence field = (MathSequence) container.getArgument(container.getInsertIndex());
-
-        // pass scripts first
-        while (currentOffset > 0 && currentField.isScript(currentOffset - 1)) {
-            MathFunction character = (MathFunction) currentField.getArgument(currentOffset - 1);
-            currentField.delArgument(currentOffset - 1);
-            currentOffset--;
-            field.addArgument(0, character);
-        }
-
-        if (currentOffset > 0) {
-            // if previous sequence argument are braces pass their content
-            if (currentField.getArgument(currentOffset - 1) instanceof MathArray) {
-
-                MathArray array = (MathArray) currentField.getArgument(currentOffset - 1);
-                currentField.delArgument(currentOffset - 1);
-                currentOffset--;
-                if (field.size() == 0) {
-                    // here we already have sequence, just set it
-                    container.setArgument(container.getInsertIndex(), array.getArgument(0));
-                } else {
-                    field.addArgument(0, array);
-                }
-
-                // if previous sequence argument is, function pass it
-            } else if (currentField.getArgument(currentOffset - 1) instanceof MathFunction) {
-
-                MathFunction function = (MathFunction) currentField.getArgument(currentOffset - 1);
-                currentField.delArgument(currentOffset - 1);
-                currentOffset--;
-                field.addArgument(0, function);
-
-                // otherwise pass character sequence
-            } else {
-
-                passCharacters(editorState, container);
-                currentOffset = editorState.getCurrentOffset();
-            }
-        }
-        editorState.setCurrentOffset(currentOffset);
-    }
-
-    private void passCharacters(EditorState editorState, MathContainer container) {
-        int currentOffset = editorState.getCurrentOffset();
-        MathSequence currentField = editorState.getCurrentField();
-        // get pass to argument
-        MathSequence field = (MathSequence) container.getArgument(container.getInsertIndex());
-
-        while (currentOffset > 0 && currentField.getArgument(currentOffset - 1) instanceof MathCharacter) {
-
-            MathCharacter character = (MathCharacter) currentField.getArgument(currentOffset - 1);
-            if (character.isOperator()) {
-                break;
-            }
-            currentField.delArgument(currentOffset - 1);
-            currentOffset--;
-            field.addArgument(0, character);
-        }
-        editorState.setCurrentOffset(currentOffset);
     }
 
     private String readCharacters(EditorState editorState) {
